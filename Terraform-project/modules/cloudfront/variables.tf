cc05f986009c6376da1855044c263b7cd0026af3
kumarshiv93 <shivkumardixit620@gmail.com>
2026-03-24 12:59:39 +0530

    Create variables.tf

---
+++ Terraform-project/modules/cloudfront/variables.tf
@@ -0,0 +1,15 @@
+variable "bucket_name" {
+  type = string
+}
+
+variable "alb_dns" {
+  type = string
+}
+
+variable "domain_name" {
+  type = string
+}
+
+variable "certificate_arn" {
+  type = string
+}
